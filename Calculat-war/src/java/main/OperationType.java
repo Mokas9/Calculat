e36b158564c6099e76bfa89037e2e44f79f31535
Mokas9 <maksim_grushevskiy@mail.ru>
2017-06-13 20:47:25 +0300

    Project

---
+++ Calculat-war/src/java/main/OperationType.java
@@ -0,0 +1,20 @@
+package main;
+
+
+public enum OperationType {
+    ADD("+"),
+    SUBTRACT("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
+    
+    private String stringValue;
+    
+    private OperationType(String stringValue){
+        this.stringValue = stringValue;
+    }
+    
+    public String getStringValue(){
+        return stringValue;
+    }
+
+}
